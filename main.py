24d0c286fd730bdd1080b146ee2cce81fa30600a
Alejandro Hernandez Toro <1006009372@u.icesi.edu.co>
2022-11-17 12:06:19 -0500

    initiated front end

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import math
 import array as arr
+from flask import json, flask, request
+from flask_cors import CORS
 from scipy.spatial import distance
 
 import matplotlib.pyplot as plt
@@ -12,9 +14,10 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
+CORS(app)
 
 data_base = pd.read_csv('data/database.csv', encoding="utf-8")
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello_world():
     return "<p>Hello, World!</p>"
