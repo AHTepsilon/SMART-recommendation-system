5a1c89b657e8925decd94acd4556e87d380fd7b0
AHTepsilon <alehertoro725@gmail.com>
2022-11-28 14:56:10 -0500

    added the other HTML files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, json, request, render_template
+from flask import Flask, json, request, render_template, url_for, redirect
 from flask_cors import CORS
 import numpy as np
 import pandas as pd
@@ -26,6 +26,30 @@ def table():
     data = pd.read_csv('data/responses2.csv')
     return render_template('index.html', tables=[data.to_html()], titles=[''])
   
+
+@app.route('/grupos', methods=['GET', 'POST'])
+def grupos():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('index'))
+
+    # show the form, it wasn't submitted
+    return render_template('grupo.html')
+
+@app.route('/lista', methods=['GET', 'POST'])
+def lista():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('index'))
+
+    # show the form, it wasn't submitted
+    return render_template('lista.html')
   
 if __name__ == "__main__":
     app.run(host="localhost", port=int("5000"))
